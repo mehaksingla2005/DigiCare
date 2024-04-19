64b99483500462c1c0a7a9aad4d5b2ddcc9b1fca
therishabhmittal05 <rishabhmittal928@gmail.com>
2024-04-19 12:08:21 +0530

    SignIn and SignUp done
    Get to post request changed

--- Backend/main.js
+++ Backend/main.js
@@ -1,31 +1,33 @@
-const express = require('express');
-const jwt = require('jsonwebtoken');
-require('dotenv').config();
+const express = require("express");
+const jwt = require("jsonwebtoken");
+require("dotenv").config();
 const app = express();
-const zodInput = require('./verification/zodInput');
+const zodInput = require("./verification/zodInput");
 const { signUpInput, signInInput } = zodInput;
 
 app.use(express.json());
 
-app.get("/signup", (req, res)=>{
-    const { email, password, name, age} = req.body
-    const result = signUpInput.safeParse({email, password, name, age});
-    const token = jwt.sign({email}, process.env.JWT_PASSWORD)
-    if(result.success){
-        res.sendStatus(200).json({
-            message: "Welcome"
-        })
-    }
-})
+app.post("/signup", (req, res) => {
+  const { email, password, name, age } = req.body;
+  const result = signUpInput.safeParse({ email, password, name, age });
+  const token = jwt.sign({ email }, process.env.JWT_PASSWORD);
+  if (result.success) {
+    res.sendStatus(200).json({
+      message: "Welcome",
+    });
+  }
+});
 
-app.get("/signin", (req, res)=>{
-    const { email, password} = req.body
-    const result = signInInput.safeParse({email, password});
-    if(result.success){
-        res.sendStatus(200).json({
-            message: "DigiCare Portal"
-        })
-    }   
-})
+app.post("/signin", (req, res) => {
+  const token = req.headers.authorization;
+  const decoded = jwt.verify(token, process.env.JWT_PASSWORD);
+  const { email, password } = req.body;
+  const result = signInInput.safeParse({ email, password });
+  if (result.success && decoded.email === email) {
+    res.sendStatus(200).json({
+      message: "DigiCare Portal",
+    });
+  }
+});
 
-app.listen(3000)
+app.listen(3000);
